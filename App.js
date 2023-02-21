df4a448c2c114bbc2b54e74a42edea79d29ba7e9
Shah fahad <shah.fahad@vorksol.com>
2023-02-21 07:44:05 +0500

    updated

--- App.js
+++ App.js
@@ -1,35 +1,38 @@
-import React, { useState } from 'react';
-import { StyleSheet, SafeAreaView } from 'react-native';
-import * as WebBrowser from 'expo-web-browser';
-import { Button } from './src/components/Button';
-import Color from './Utils/colors';
+import { City } from "country-state-city";
+import React, { useEffect, useState } from "react";
+import { View, Text, TextInput, StyleSheet, Button } from "react-native";
 
-export default function App() {
-  const [result, setResult] = useState(null);
+const App = () => {
+  const [cityName, setCityName] = useState("");
+  const [cityAbbreviation, setCityAbbreviation] = useState(null);
+  console.log("🚀 ~ file: App.js:7 ~ App ~ cityName", cityName);
+  // useEffect(() => {
 
-  const _handlePressButtonAsync = async () => {
-    let result = await WebBrowser.openBrowserAsync('https://tp.media/r?marker=408934&trs=209963&p=5976&u=https%3A%2F%2Fwayaway.io&campaign_id=200');
-    setResult(result);
+  // }, [cityName]);
+  const onSubmit = () => {
+    const cities = City.getAllCities();
+    const city = cities.find((city) => city.name === cityName);
+    setCityAbbreviation(city ? city.abbreviation : null);
+    console.log("DITY===", cityAbbreviation);
   };
-
   return (
-    <SafeAreaView style={styles.container}>
-      <Button
-        btnText="Search through browser"
-        onPress={_handlePressButtonAsync}
-        backgroundColor={Color.blueColor}
-        btnColor={Color.whiteColor}
+    <View style={styles.container}>
+      {/* <TextInput
+        placeholder="City name"
+        value={cityName}
+        onChangeText={setCityName}
       />
-    </SafeAreaView>
+      <Text>City abbreviation: {cityAbbreviation || "N/A"}</Text>
+      <Button title="submit" onPress={onSubmit} /> */}
+      <Text>hi</Text>
+    </View>
   );
-}
-
+};
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    alignItems: 'center',
-    justifyContent: 'center',
-    paddingTop: 10,
-    backgroundColor: '#ecf0f1',
+    alignItems: "center",
+    justifyContent: "center",
   },
 });
+export default App;
